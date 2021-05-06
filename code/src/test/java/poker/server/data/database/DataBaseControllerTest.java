852db355df9de4d91c7e7b22279b0037a0fee59b
lilartisto <61553146+lilartisto@users.noreply.github.com>
2021-05-06 16:22:15 +0200

    DataBaseController.getPlayer method test

--- code/src/test/java/poker/server/data/database/DataBaseControllerTest.java
+++ code/src/test/java/poker/server/data/database/DataBaseControllerTest.java
@@ -3,10 +3,12 @@ package poker.server.data.database;
 import org.junit.jupiter.api.Test;
 import poker.server.data.Player;
 
+import javax.xml.crypto.Data;
 import java.sql.SQLException;
 import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class DataBaseControllerTest {
 
@@ -26,9 +28,9 @@ public class DataBaseControllerTest {
     }
 
     @Test
-    public void shouldInsertPlayerToDatabaseWhenDatabaseDoesWorks(){
+    public void shouldInsertPlayerToDatabaseWhenDatabaseWorks(){
         try {
-            DataBaseController dbController = new DataBaseController(
+             DataBaseController dbController = new DataBaseController(
                     "jdbc:postgresql://localhost:7432/poker_database",
                     "poker_user",
                     "Fl94yuwHClB6eKltjLnPYQ=="
@@ -43,6 +45,7 @@ public class DataBaseControllerTest {
 
             assertEquals(expectedPlayer.nickname, actualPlayer.nickname);
             assertEquals(expectedPlayer.getMoney(), actualPlayer.getMoney());
+            dbController.closeConnection();
         } catch (SQLException e){
             e.printStackTrace();
             assert false;
@@ -50,7 +53,7 @@ public class DataBaseControllerTest {
     }
 
     @Test
-    public void shouldUpdatePlayerToDatabaseWhenDatabaseDoesWorks(){
+    public void shouldUpdatePlayerToDatabaseWhenDatabaseWorks(){
         try {
             DataBaseController dbController = new DataBaseController(
                     "jdbc:postgresql://localhost:7432/poker_database",
@@ -75,6 +78,28 @@ public class DataBaseControllerTest {
 
             assertEquals(expectedPlayer.nickname, actualPlayer.nickname);
             assertEquals(expectedPlayer.getMoney(), actualPlayer.getMoney());
+            dbController.closeConnection();
+        } catch (SQLException e){
+            e.printStackTrace();
+            assert false;
+        }
+    }
+
+    @Test
+    public void shouldReturnNullWhenDatabaseWorksAndDoesNotIncludePlayer(){
+        try {
+            DataBaseController dbController = new DataBaseController(
+                    "jdbc:postgresql://localhost:7432/poker_database",
+                    "poker_user",
+                    "Fl94yuwHClB6eKltjLnPYQ=="
+            );
+            String randomNickname = "M" + new Random().nextInt((int)1e6);
+
+            Player actualPlayer = dbController.getPlayer(randomNickname);
+
+            assertNull(actualPlayer);
+
+            dbController.closeConnection();
         } catch (SQLException e){
             e.printStackTrace();
             assert false;
