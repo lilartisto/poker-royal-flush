92ae84730e398c5c16890829e64b5da667a4bc96
lilartisto <61553146+lilartisto@users.noreply.github.com>
2021-05-06 16:17:45 +0200

    DataBaseController - inserting, updating, getting methods + tests

--- code/src/main/java/poker/server/data/database/DataBaseController.java
+++ code/src/main/java/poker/server/data/database/DataBaseController.java
@@ -2,47 +2,63 @@ package poker.server.data.database;
 
 import poker.server.data.Player;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class DataBaseController {
 
     private Connection connection;
-    private Statement statement;
+    //private Statement statement;
 
-    public DataBaseController(String url, String username, String password) {
-        try{
-            connection = DriverManager.getConnection(url, username, password);
-            System.out.println("Connected to database");
-        } catch (SQLException e) {
-            e.printStackTrace();
-            //throw new IllegalArgumentException("Cannot connect to database");
+    public DataBaseController(String url, String username, String password) throws SQLException{
+        connection = DriverManager.getConnection(url, username, password);
+    }
+
+    public void insertPlayer(Player player) throws SQLException{
+        String sql = "INSERT INTO T_PLAYERS (nickname, money) VALUES (?, ?)";
+        PreparedStatement statement = connection.prepareStatement(sql);
+
+        statement.setString(1, player.nickname);
+        statement.setInt(2, player.getMoney());
+
+        if(statement.executeUpdate() <= 0){
+            throw new SQLException("Cannot insert " + player.nickname + " to database");
         }
     }
 
-    public void createTable() {
-        throw new UnsupportedOperationException("Not implemented yet");
+    public void updatePlayer(Player player) throws SQLException{
+        String sql = "UPDATE T_PLAYERS SET money = ? WHERE nickname = ?";
+        PreparedStatement statement = connection.prepareStatement(sql);
+
+        statement.setInt(1, player.getMoney());
+        statement.setString(2, player.nickname);
+
+        if(statement.executeUpdate() <= 0){
+            throw new SQLException("Cannot update" + player.nickname + "in database");
+        }
     }
 
-    public void insertPlayer(Player player) {
-        throw new UnsupportedOperationException("Not implemented yet");
-    }
+    public Player getPlayer(String nickname) throws SQLException{
+        String sql = "SELECT * FROM T_PLAYERS WHERE nickname = ?";
+        PreparedStatement statement = connection.prepareStatement(sql);
+        statement.setString(1, nickname);
 
-    public Player getPlayer(String nickname) {
-        throw new UnsupportedOperationException("Not implemented yet");
-    }
+        ResultSet result = statement.executeQuery();
 
-    public void deletePlayer(Player player) {
-        throw new UnsupportedOperationException("Not implemented yet");
+        if(result.next()){
+            int money = result.getInt("money");
+            Player player = new Player(nickname);
+            player.setMoney(money);
+            return player;
+        } else {
+            return null;
+        }
     }
 
     public void closeConnection(){
         try {
             connection.close();
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
     }
 }
--- code/src/test/java/poker/server/data/database/DataBaseControllerTest.java
+++ code/src/test/java/poker/server/data/database/DataBaseControllerTest.java
@@ -1,11 +1,83 @@
 package poker.server.data.database;
 
 import org.junit.jupiter.api.Test;
+import poker.server.data.Player;
+
+import java.sql.SQLException;
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DataBaseControllerTest {
 
     @Test
-    public void shouldConnectToDatabase(){
-        new DataBaseController("jdbc:postgresql://localhost:5432/poker", "poker_modifier", "7hd42caj").closeConnection();
+    public void shouldConnectToDatabaseWhenDatabaseWorks() {
+        try {
+            new DataBaseController(
+                    "jdbc:postgresql://localhost:7432/poker_database",
+                    "poker_user",
+                    "Fl94yuwHClB6eKltjLnPYQ=="
+            ).closeConnection();
+            assert true;
+        } catch (SQLException e){
+            e.printStackTrace();
+            assert false;
+        }
+    }
+
+    @Test
+    public void shouldInsertPlayerToDatabaseWhenDatabaseDoesWorks(){
+        try {
+            DataBaseController dbController = new DataBaseController(
+                    "jdbc:postgresql://localhost:7432/poker_database",
+                    "poker_user",
+                    "Fl94yuwHClB6eKltjLnPYQ=="
+            );
+
+            Player expectedPlayer = new Player("M" + new Random().nextInt((int)1e6));
+            expectedPlayer.setMoney(500);
+
+            dbController.insertPlayer(expectedPlayer);
+
+            Player actualPlayer = dbController.getPlayer(expectedPlayer.nickname);
+
+            assertEquals(expectedPlayer.nickname, actualPlayer.nickname);
+            assertEquals(expectedPlayer.getMoney(), actualPlayer.getMoney());
+        } catch (SQLException e){
+            e.printStackTrace();
+            assert false;
+        }
+    }
+
+    @Test
+    public void shouldUpdatePlayerToDatabaseWhenDatabaseDoesWorks(){
+        try {
+            DataBaseController dbController = new DataBaseController(
+                    "jdbc:postgresql://localhost:7432/poker_database",
+                    "poker_user",
+                    "Fl94yuwHClB6eKltjLnPYQ=="
+            );
+            Player expectedPlayer = new Player("M" + new Random().nextInt((int)1e6));
+
+            expectedPlayer.setMoney(500);
+            dbController.insertPlayer(expectedPlayer);
+
+            Player actualPlayer = dbController.getPlayer(expectedPlayer.nickname);
+
+            assertEquals(expectedPlayer.nickname, actualPlayer.nickname);
+            assertEquals(expectedPlayer.getMoney(), actualPlayer.getMoney());
+
+
+            expectedPlayer.setMoney(100);
+            dbController.updatePlayer(expectedPlayer);
+
+            actualPlayer = dbController.getPlayer(expectedPlayer.nickname);
+
+            assertEquals(expectedPlayer.nickname, actualPlayer.nickname);
+            assertEquals(expectedPlayer.getMoney(), actualPlayer.getMoney());
+        } catch (SQLException e){
+            e.printStackTrace();
+            assert false;
+        }
     }
 }
